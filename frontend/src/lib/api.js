a4cce65f6cbe1081feb2114324ce4fa9214e2b51
madanadatta <147394640+MadangitHub3101@users.noreply.github.com>
2026-04-25 13:00:35 +0530

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,9 +1,6 @@
 import axios from 'axios'
 
-const BASE = process.env.NEXT_PUBLIC_API_BASE || 'http://localhost'
-
-export const cartAPI = axios.create({ baseURL: `${BASE}/api/cart` })
-export const searchAPI = axios.create({ baseURL: `${BASE}/api/search` })
-export const paymentAPI = axios.create({ baseURL: `${BASE}/api/pay` })
-export const wishlistAPI = axios.create({ baseURL: `${BASE}/api/wishlist` })
-export const recAPI = axios.create({ baseURL: `${BASE}/api/rec` })
+export const cartAPI     = axios.create({ baseURL: process.env.NEXT_PUBLIC_CART_URL })
+export const searchAPI   = axios.create({ baseURL: process.env.NEXT_PUBLIC_SEARCH_URL })
+export const wishlistAPI = axios.create({ baseURL: process.env.NEXT_PUBLIC_WISHLIST_URL })
+export const recAPI      = axios.create({ baseURL: process.env.NEXT_PUBLIC_REC_URL })
