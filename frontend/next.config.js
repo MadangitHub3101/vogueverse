a83a10b97361db7480a46838435dec5b928bd1c2
madanadatta <147394640+MadangitHub3101@users.noreply.github.com>
2026-04-25 13:01:51 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  env: {
-    NEXT_PUBLIC_API_BASE: process.env.NEXT_PUBLIC_API_BASE,
+ env: {
+  NEXT_PUBLIC_CART_URL:     process.env.NEXT_PUBLIC_CART_URL,
+  NEXT_PUBLIC_SEARCH_URL:   process.env.NEXT_PUBLIC_SEARCH_URL,
+  NEXT_PUBLIC_WISHLIST_URL: process.env.NEXT_PUBLIC_WISHLIST_URL,
+  NEXT_PUBLIC_REC_URL:      process.env.NEXT_PUBLIC_REC_URL,
+  NEXT_PUBLIC_PAYMENT_URL:  process.env.NEXT_PUBLIC_PAYMENT_URL,
   },
   images: {
     domains: ['images.unsplash.com'],
